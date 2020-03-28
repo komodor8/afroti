dd5b8ba1ef0dec53f656b5614a8f8285bacca9bb
= <=>
2020-03-28 15:39:50 +0100

    fix styles

--- personal/urls.py
+++ personal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexSplashScreen.as_view(), name='index'),
+    path('home/', views.IndexSplashScreen.as_view(), name='index'),
     path('plats-a-emporter/', views.IndexFood.as_view(), name='plats-a-emporter'),
     path('gateaux/', views.IndexCake.as_view(), name='gateaux'),
 ]
